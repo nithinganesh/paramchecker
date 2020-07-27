19a72c616c5da0eabe684cfad57408d5859f4c31
nithinganesh <60597123+nithinganesh@users.noreply.github.com>
2020-07-27 17:16:18 +0530

    Fix cyclomatic complexity error

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,11 +1,20 @@
 
+
+bool isBpOk(float bpm)
+{
+  return (bpm < 70 || bpm > 150);
+}
+
+bool isSpo2Ok(float spo2)
+{
+  return (spo2 < 80);
+}
+
+bool isrespRateOk(float respRate)
+{
+  return (respRate < 30 || respRate > 60);
+}
+
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  if(bpm < 70 || bpm > 150) {
-    return false;
-  } else if(spo2 < 80) {
-    return false;
-  } else if(respRate < 30 || respRate > 60) {
-    return false;
-  }
-  return true;
+  return (isBpOk(bpm) || isSpo2Ok(spo2) || isrespRateOk(respRate));
 }
