f47a03af12a871a78151e3b2beaee3f9512de857
nithinganesh <60597123+nithinganesh@users.noreply.github.com>
2020-07-27 17:26:24 +0530

    Fix error

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -2,17 +2,17 @@
 
 bool isBpOk(float bpm)
 {
-  return (bpm < 70 || bpm > 150);
+  return !(bpm < 70 || bpm > 150);
 }
 
 bool isSpo2Ok(float spo2)
 {
-  return (spo2 < 80);
+  return !(spo2 < 80);
 }
 
 bool isrespRateOk(float respRate)
 {
-  return (respRate < 30 || respRate > 60);
+  return !(respRate < 30 || respRate > 60);
 }
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
